1767a37e7b305659af202ee17e4b99007b1d5ebf
Sebastian Ames <sebastianames3@gmail.com>
2025-10-01 20:37:27 -0400

    feat(app): render journal section in Journal tab

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import os
 import streamlit as st
 from src.settings import Settings
-from src.ui.components import header, journal_sidebar, data_section
+from src.ui.components import header, journal_sidebar, data_section, journal_section
 
 def main():
     st.set_page_config(page_title='AI Trader / Journal', layout='wide')
@@ -10,11 +9,11 @@ def main():
     settings = Settings.from_env()
     journal_sidebar()
 
-    tab1, tab2 = st.tabs(["Data", "Journal"])  # TODO: add journal UI
+    tab1, tab2 = st.tabs(["Data", "Journal"])
     with tab1:
         data_section(settings=settings)
     with tab2:
-        st.info("Journal UI coming soon. Create/read/update entries with tags.")
+        journal_section()
 
 if __name__ == "__main__":
     main()
